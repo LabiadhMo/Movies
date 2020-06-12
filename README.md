657ccdc6399a20533cdaf0b6c285ba649ea13e1a
mohamed labiadh <labiadhmo@gmail.com>
2020-06-12 20:34:39 +0100

    pro

---
+++ README.md
@@ -0,0 +1,2 @@
+# Movies App made in Flutter with api data from TMDB 
+ This is an app that displays you details of movies that you can search for or browse.<br>
---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "API_KEY_HERE";
+const String TMDB_BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
